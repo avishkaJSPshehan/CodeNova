3bebd817ff295182c4de5bd52999bfc13f88b911
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-01-03 00:08:56 +0530

    Add Css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { SignedIn ,SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-    {/* Testing Route */}
       <SignedOut>
         <SignUpButton />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white front-bold py-2 px-4 rounded">
+          Sign Up
+        </button>
       </SignedOut>
 
       <SignedIn>
