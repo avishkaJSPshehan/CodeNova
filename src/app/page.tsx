a8160de87589cd54d34c877c8d212c1d5300ba1b
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-01-03 00:00:52 +0530

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { SignedIn ,SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+    {/* Testing Route */}
       <SignedOut>
         <SignUpButton />
       </SignedOut>
